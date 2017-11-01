c7d6eb99a8e511b9206938c8c9dd3bd2339a17d9
DESKTOP-RAAFD5A\adelinetush <adelinetush@gmail.com>
2017-11-01 15:25:02 +0300

    Changed Port for Python

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #!flask/bin/python
+# 
+import os 
 from app import app
-#app.run(debug=True)
 
+#app.run(debug=True)
+port = int(os.environ.get('PORT',5000))
 if(__name__=="__main__"):
-    app.run(host='0.0.0.0',port=33507)
+    app.run(host='0.0.0.0',port=port)
